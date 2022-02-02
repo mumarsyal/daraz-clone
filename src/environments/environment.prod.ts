97fe6575c77d49bb5029fab191d0afe99fc50d4e
zeeshan-hazelsoft <81971878+zeeshan-hazelsoft@users.noreply.github.com>
2022-02-02 13:23:13 +0500

    some changes for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+	production: true,
+	apiUrlPrefix:
+		'http://darazcloneapi-env.eba-fwn3euy2.us-east-1.elasticbeanstalk.com/api/',
+	maxFilesAllowedToUpload: 10,
 };
